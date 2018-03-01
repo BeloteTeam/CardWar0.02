c04584058d067ee1955064486f0d59d956d7a927
root <root@abv.bg>
2018-03-01 16:27:25 +0200

    CardWar 0.02

--- Card Game/CardWar.py
+++ Card Game/CardWar.py
@@ -1,5 +1,4 @@
-#shte trqbva da import tkinter, math (za button i za poziciqta na mishkata)
-import os,pygame,math
+import os, pygame, math
 import tkinter as tk
 import random
 from Deck import Deck
@@ -16,38 +15,29 @@ def text_objects(text, font):
     textSurface = font.render(text, True, white)
     return textSurface, textSurface.get_rect()
 
+
 def text_objects2(text, font):
     textSurface2 = font.render(text, True, black)
     return textSurface2, textSurface2.get_rect()
 
 
-#display-a na koito se pokazva igrata, glavniq class(trqbva da sedi nai otgore)
 class CardWar:
 
     def __init__(self):
         pass
-        #1
+
         pygame.init()
-        #2
+
         self.gameDisplay = pygame.display.set_mode((width, height))
         pygame.display.set_caption("CardWar")
-        #3
+
         self.clock = pygame.time.Clock()
-        #4
-        self.gameDisplay.fill(white)                                               
-        smallText = pygame.font.Font("freesansbold.ttf",20)                        
-        textSurf, textRect = text_objects2("Click anywhere to start!", smallText)  
+
+        self.gameDisplay.fill(black)
+        smallText = pygame.font.Font("freesansbold.ttf", 20)
+        textSurf, textRect = text_objects("Click anywhere to start!", smallText)
         textRect.center = (height/2), (width/2)                                    
         self.gameDisplay.blit(textSurf, textRect)                                  
-
-    def game_pause(self):
-
-        pause = True
-
-        while pause:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    quit()
 
     def update(self):
         self.clock.tick(60)
@@ -56,14 +46,26 @@ class CardWar:
             if event.type == pygame.QUIT:
                 exit()
 
-            if event.type == pygame.MOUSEBUTTONDOWN:
+            import time
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_ESCAPE]:
                 self.gameDisplay.fill(black)
-                pygame.draw.rect(self.gameDisplay, green, (50,50,80,50))
-                smallText = pygame.font.Font("freesansbold.ttf",20)
+                smallText = pygame.font.Font("freesansbold.ttf", 40)
+                textSurf, textRect = text_objects("Game Over", smallText)
+                textRect.center = (width / 2), (height / 2)
+                self.gameDisplay.blit(textSurf, textRect)
+                pygame.display.update()
+                time.sleep(1)
+                quit()
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                self.gameDisplay.fill(white)
+                pygame.draw.rect(self.gameDisplay, green, (50, 50, 80, 50))
+                smallText = pygame.font.Font("freesansbold.ttf", 20)
                 textSurf, textRect = text_objects2("Yes!", smallText)
                 textRect.center = (50+80/2), (50+50/2)
                 self.gameDisplay.blit(textSurf, textRect)
-                smallText = pygame.font.Font("freesansbold.ttf",40)
+                smallText = pygame.font.Font("freesansbold.ttf", 40)
                 textSurf, textRect = text_objects("Are you ready?", smallText)
                 textRect.center = (width/2), (height/2)
                 self.gameDisplay.blit(textSurf, textRect)
@@ -74,7 +76,7 @@ class CardWar:
 
             if 50+80 > mouse[0] > 80 and 50+50 > mouse[1] > 50:
                 if click[0] == 1:
-                    card = random.randint(0,len(Deck)-1)
+                    card = random.randint(0, len(Deck)-1)
                     print(Deck[card])
 
         pygame.display.flip()
@@ -83,5 +85,3 @@ class CardWar:
 bg = CardWar()
 while 1:
     bg.update()
-
-
