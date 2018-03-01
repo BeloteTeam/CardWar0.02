82b6c4d3b363d99481b2faa89a940711ee2c28ea
root <root@abv.bg>
2018-03-01 22:49:11 +0200

    CardWar0.02

--- Card Game/CardWar.py
+++ Card Game/CardWar.py
@@ -5,7 +5,7 @@ from Deck import Deck
 
 white = [255, 255, 255]
 black = [0, 0, 0]
-green = [0, 240, 0]
+green = [0, 200, 0]
 
 width, height = 400, 400
 bheight, bwidth = 80, 100
@@ -33,51 +33,64 @@ class CardWar:
 
         self.clock = pygame.time.Clock()
 
+
         self.gameDisplay.fill(black)
         smallText = pygame.font.Font("freesansbold.ttf", 20)
         textSurf, textRect = text_objects("Click anywhere to start!", smallText)
-        textRect.center = (height/2), (width/2)                                    
-        self.gameDisplay.blit(textSurf, textRect)                                  
+        textRect.center = (height/2), (width/2)
+        self.gameDisplay.blit(textSurf, textRect)
 
     def update(self):
         self.clock.tick(60)
 
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+            if event.type == pygame.QUIT: 
                 exit()
-
-            import time
-            keys = pygame.key.get_pressed()
-            if keys[pygame.K_ESCAPE]:
-                self.gameDisplay.fill(black)
-                smallText = pygame.font.Font("freesansbold.ttf", 40)
-                textSurf, textRect = text_objects("Game Over", smallText)
-                textRect.center = (width / 2), (height / 2)
-                self.gameDisplay.blit(textSurf, textRect)
-                pygame.display.update()
-                time.sleep(1)
-                quit()
-
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                self.gameDisplay.fill(white)
-                pygame.draw.rect(self.gameDisplay, green, (50, 50, 80, 50))
-                smallText = pygame.font.Font("freesansbold.ttf", 20)
-                textSurf, textRect = text_objects2("Yes!", smallText)
-                textRect.center = (50+80/2), (50+50/2)
-                self.gameDisplay.blit(textSurf, textRect)
-                smallText = pygame.font.Font("freesansbold.ttf", 40)
-                textSurf, textRect = text_objects("Are you ready?", smallText)
-                textRect.center = (width/2), (height/2)
-                self.gameDisplay.blit(textSurf, textRect)
-                pygame.display.update()
 
             mouse = pygame.mouse.get_pos()
             click = pygame.mouse.get_pressed()
+            import time
+            game_start = False
 
-            if 50+80 > mouse[0] > 80 and 50+50 > mouse[1] > 50:
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                self.gameDisplay.fill(white)
+                pygame.draw.rect(self.gameDisplay, green, (100, 150, 200, 50))
+                smallText = pygame.font.Font("freesansbold.ttf", 20)
+                textSurf, textRect = text_objects2("Start Game", smallText)
+                textRect.center = (100+200/2), (150+50/2)
+                self.gameDisplay.blit(textSurf, textRect)
+                smallText = pygame.font.Font("freesansbold.ttf", 40)
+                textSurf, textRect = text_objects2("Card Wars", smallText)
+                textRect.center = (width/2), (height-350)
+                self.gameDisplay.blit(textSurf, textRect)
+                pygame.display.update()
+
+            background = pygame.image.load("belote.png")
+
+            if 100+200 > mouse[0] > 100 and 150+50 > mouse[1] > 150:
                 if click[0] == 1:
-                    card = random.randint(0, len(Deck)-1)
-                    print(Deck[card])
+                    game_start = True
+                        
+            while game_start == True:
+                self.gameDisplay.fill(black)
+                self.gameDisplay.blit(background, (0, 0))
+                pygame.display.update()
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        quit()
+                keys = pygame.key.get_pressed()
+                if keys[pygame.K_ESCAPE]:
+                    self.gameDisplay.fill(black)
+                    smallText = pygame.font.Font("freesansbold.ttf", 30)
+                    textSurf, textRect = text_objects("Game Over", smallText)
+                    textRect.center = (width / 2), ((height / 2)-10)
+                    self.gameDisplay.blit(textSurf, textRect)
+                    smallText = pygame.font.Font("freesansbold.ttf", 15)
+                    textSurf, textRect = text_objects("(Click to go back)", smallText)
+                    textRect.center = (width / 2), ((height / 2)+20)
+                    self.gameDisplay.blit(textSurf, textRect)
+                    pygame.display.update()
+                    game_start = False
 
         pygame.display.flip()
 
