13eaaacccfc9bfc124e5d76202aee0454f6e7aae
root <root@abv.bg>
2018-03-01 10:52:39 +0200

    CardWar 0.02

--- Card Game/CardWar.py
+++ Card Game/CardWar.py
@@ -3,6 +3,7 @@ import os,pygame,math
 import tkinter as tk
 import random
 
+gamestart = False
 
 white = [255,255,255]
 black = [0,0,0]
@@ -15,7 +16,7 @@ def text_objects(text, font):
 	return textSurface, textSurface.get_rect()
 
 #display-a na koito se pokazva igrata, glavniq class(trqbva da sedi nai otgore)
-class CardWar():		
+class CardWar():
     def __init__(self):
         pass
         #1
@@ -25,17 +26,30 @@ class CardWar():
         self.gameDisplay = pygame.display.set_mode((width, height))
         pygame.display.set_caption("CardWar")
         #3
-        self.clock=pygame.time.Clock()        
+        self.clock=pygame.time.Clock()
+			        
                
     def update(self):
         self.clock.tick(60)
 
-        self.gameDisplay.fill(white)
-        pygame.draw.rect(self.gameDisplay, (0,230,0), (bwidth,40,bheight,40))
-        smallText = pygame.font.Font("freesansbold.ttf",20)
-        textSurf, textRect = text_objects("GO!", smallText)
-        textRect.center = ( (bwidth+(bheight/2)), (40+(40/2)) )
-        self.gameDisplay.blit(textSurf, textRect)
+        
+
+        while gamestart = False:
+            self.gameDisplay.fill(black)
+            smallText = pygame.font.Font("freesansbold.ttf", 120)
+            textSurf,textRect = text_objects("Click anywhere to begin!", smallText)
+            textRect.center = (self.gameDisplay,(400+(400/2)), (40+(40/2)))
+            self.gameDisplay.blit(textSurf, textRect)
+            
+            for event in pygame.event.get():
+                if event.type == pygame.mouse.get_pressed():
+                    gamestart = True
+                    self.gameDisplay.fill(white)
+                    pygame.draw.rect(self.gameDisplay, (0,230,0), (bwidth,40,bheight,40))
+                    smallText = pygame.font.Font("freesansbold.ttf",20)
+                    textSurf, textRect = text_objects("GO!", smallText)
+                    textRect.center = ( (bwidth+(bheight/2)), (40+(40/2)) )
+                    self.gameDisplay.blit(textSurf, textRect)
         
         
         
